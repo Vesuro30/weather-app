e00eb61e0c21a6f25604285c15abfd7b45fd4cfa
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-05-25 21:16:13 -0600

    initial test commit

---
+++ script/script.js
@@ -0,0 +1,20 @@
+//
+//
+//
+
+var url = 'https://api.openweathermap.org/data/2.5/weather?q=Denver,us&APPID=dd3d30ad19f4fbf80b58504cc2a578fe&units=imperial'
+
+fetch(url)
+.then(function (response) {
+  return response.json();
+  // console.log(response)
+})
+.then(function (data) {
+  // for (var i = 0; i < data.length; i++) 
+  // {
+  //   var listItem = document.createElement('li');
+  //   listItem.textContent = data[i].html_url;
+  //   repoList.appendChild(listItem);
+  // }
+  console.log(data)
+});
